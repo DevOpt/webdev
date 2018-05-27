24de253f7ae09922562747cae1b40b4274662bea
Abdurahman Sherif <abdurahman.sherif@gmail.com>
2018-05-27 17:34:20 -0400

    init

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+
+    // PRELOADER
+    $('#preloader').delay(500).fadeOut('slow'); // will fade out the white DIV that covers the website.
+
+        // SCROLL ANIMATION
+        smoothScroll.init({
+            speed: 1000,
+            easing: 'easeInOutCubic',
+            updateURL: false,
+            offset: 0,
+            callbackBefore: function(toggle, anchor) {},
+            callbackAfter: function(toggle, anchor) {}
+        });
+
+}); // document ready end
+
+
+"use strict";
+$(window).load(function () {
+
+
+
+
+
+}); // window load end 
